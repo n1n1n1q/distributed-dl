a8937c8e6f7f91d12f2c98ab29bdbd544eb83505
Oleh Basystyi <basystyo@gmail.com>
2025-03-20 19:52:20 +0200

    non-blocking communication

--- src/distributed.hpp
+++ src/distributed.hpp
@@ -16,6 +16,16 @@ inline void recv(boost::mpi::communicator &world, torch::Tensor &tensor, int sou
     received.toTensor(tensor);
 }
 
+boost::mpi::request isend(boost::mpi::communicator &world, torch::Tensor &tensor, int dest) {
+    SerializedTensor tens{tensor};
+    return world.isend(dest, 0, *tens);
+}
+
+boost::mpi::request irecv(boost::mpi::communicator &world, torch::Tensor &tensor, int source) {
+    SerializedTensor received{torch::zeros_like(tensor)};
+    return world.irecv(source, 0, *received);
+}
+
 inline void all_reduce(boost::mpi::communicator &world, torch::Tensor &tensor, int dest = 0) {
     int rank = world.rank();
     int size = world.size();
