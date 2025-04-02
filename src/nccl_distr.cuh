effec0999a6775531e23556d694085130518bd36
Anya Stasyshyn <anna.stasyshynn@gmail.com>
2025-04-03 01:23:27 +0300

    add other functions for nccl

--- src/nccl_distr.cuh
+++ src/nccl_distr.cuh
@@ -57,4 +57,52 @@ inline void all_reduce(ncclComm_t comm, cudaStream_t stream, torch::Tensor &tens
     NCCLCHECK(ncclAllReduce(data, data, size, ncclFloat, op, comm, stream));
 }
 
+inline void gather(ncclComm_t comm, cudaStream_t stream,
+    torch::Tensor &send_tensor, torch::Tensor &recv_tensor,
+    int root, int rank, int nranks) {
+    size_t count = send_tensor.numel();
+    NCCLCHECK(ncclGroupStart());
+    if (rank == root) {
+        float* src = send_tensor.data_ptr<float>();
+        float* dest = recv_tensor.data_ptr<float>() + root * count;
+        CUDACHECK(cudaMemcpyAsync(dest, src, count * sizeof(float),
+                        cudaMemcpyDeviceToDevice, stream));
+        for (int i = 0; i < nranks; i++) {
+            if (i == root) continue;
+        float* recv_ptr = recv_tensor.data_ptr<float>() + i * count;
+        NCCLCHECK(ncclRecv(recv_ptr, count, ncclFloat, i, comm, stream));
+        }
+    } else {
+        NCCLCHECK(ncclSend(send_tensor.data_ptr<float>(), count, ncclFloat, root, comm, stream));
+    }
+    NCCLCHECK(ncclGroupEnd());
+    CUDACHECK(cudaStreamSynchronize(stream));
+}
+
+inline void scatter(ncclComm_t comm, cudaStream_t stream, torch::Tensor &tensor, int root = 0) {
+    size_t count = recv_tensor.numel();
+    NCCLCHECK(ncclGroupStart());
+    if (rank == root) {
+        for (int i = 0; i < nranks; i++) {
+            float* src_ptr = send_tensor.data_ptr<float>() + i * count;
+            if (i == root) {
+                CUDACHECK(cudaMemcpyAsync(recv_tensor.data_ptr<float>(), src_ptr,
+                                          count * sizeof(float), cudaMemcpyDeviceToDevice, stream));
+            } else {
+                NCCLCHECK(ncclSend(src_ptr, count, ncclFloat, i, comm, stream));
+            }
+        }
+    } else {
+        NCCLCHECK(ncclRecv(recv_tensor.data_ptr<float>(), count, ncclFloat, root, comm, stream));
+    }
+    NCCLCHECK(ncclGroupEnd());
+    CUDACHECK(cudaStreamSynchronize(stream));
+}
+
+inline void barrier(ncclComm_t comm, ) {
+    float dummy_send = 0.0f, dummy_recv = 0.0f;
+    NCCLCHECK(ncclAllReduce(&dummy_send, &dummy_recv, 1, ncclFloat, ncclSum, comm, stream));
+    CUDACHECK(cudaStreamSynchronize(stream));
+}
+
 #endif // NCCLDISTR
