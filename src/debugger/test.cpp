2f0f196544dc22bd53247d030bca492c9bcb343a
lalkaman1981 <oleg.shtogrin2005@gmail.com>
2025-05-02 09:46:10 +0000

    small fix

--- src/debugger/test.cpp
+++ src/debugger/test.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-    MPI_Init(&argc, &argv);                 // Initialize MPI execution environment:contentReference[oaicite:2]{index=2}
+    MPI_Init(&argc, &argv);
     int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);   // Get this process’s rank in MPI_COMM_WORLD
-    MPI_Comm_size(MPI_COMM_WORLD, &size);   // Get total number of processes
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
     std::cout << "Hello from rank " << rank
-              << " of " << size << std::endl;  // Print a message
-    MPI_Finalize();                         // Clean up MPI
+              << " of " << size << std::endl;
+    MPI_Finalize(); 
     return 0;
 }
